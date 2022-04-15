fcb7836a8855632b36a6db43e3b132293799f52b
ronaldobergara <bergaraheadbanger@gmail.com>
2022-04-15 10:46:25 -0300

    Criando todos simples

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,66 @@
 <template>
-  <h1>{{ count }}</h1>
+  <p v-for="todo in doneTodos" :key="todo.text">
+    {{ todo.text }}
+  </p>
 
-  <button @click="increment">incrementar</button>
-  <button @click="decrease">Diminuir</button>
-  <button @click="reset">Resetar</button>
+  <button @click="checkAllTodos()">Finalizar</button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+// [x] Definir o dado `todos`
+// [x] Preencher os `todos` quando o componente é criado
+// [x] Exibir em tela apenas `todos` concluídos
+// [x] Ter um método para concluir os `todos`
+// [x] Mostrar um alerta quando todos os `todos` forem finalizados
+
+interface Todo {
+  text: string
+  done: boolean
+}
+
 export default defineComponent({
   data() {
     return {
-      count: 0
+      todos: [] as Todo[]
+    }
+  },
+  created() {
+    this.todos = [
+      { text: 'Estudar Typescript', done: true },
+      { text: 'Lavar os pratos', done: false },
+      { text: 'Aprender Nuxt.js', done: true }
+    ]
+  },
+  computed: {
+    doneTodos(): Todo[] {
+      return this.todos.filter((todo) => todo.done)
+    }
+  },
+  watch: {
+    todos(newValue: Todo[]) {
+      const isFinished = !newValue.some(({ done }) => !done)
+
+      if (isFinished) {
+        alert('Yuuuuuuup')
+      }
     }
   },
   methods: {
-    increment(): void {
-      this.count++
-    },
-    decrease(): void {
-      this.count--
-    },
-    reset(): void {
-      this.count = 0
+    checkAllTodos() {
+      this.todos = this.todos.map(({ text }) => {
+        return { text, done: true }
+      })
     }
   }
 })
 </script>
+
+<style>
+p {
+  font-size: larger;
+  font-weight: bold;
+  font-size: 50px;
+}
+</style>
